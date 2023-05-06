eae642b8ed98e298d4908c28930e7142f220e001
Md kamrul hassan <kamrul1157024@gmail.com>
2023-05-06 19:10:46 +0600

    update post perpage config

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "Kamrul",
   ogImage: "kamrul.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 5,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
